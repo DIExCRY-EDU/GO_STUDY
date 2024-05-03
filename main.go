1424f45432006bc6b2aea07df63ed1c62078d258
granolaa Asus TUF <max@MaxPC>
2024-05-03 17:50:59 +0300

    Global variables

--- main.go
+++ main.go
@@ -4,10 +4,28 @@ import (
 	"fmt"
 )
 
+var a, b, c int
+
 func main() {
 	// message := []byte("asd")
 	// fmt.Println(message)
 
-	var a byte = 98
-	fmt.Printf("%c\n", a)
+	// var a byte = 97
+	// fmt.Printf("%c\n", a)
+
+	// var b rune = 'b'
+	// fmt.Printf("%c\n", b)
+
+	a, b, c := 1, 2, 3
+
+	a, b = b, a
+
+	a, _, c = 0, 2, 0
+	fmt.Println(a, b, c)
 }
+
+func print() {
+	fmt.Println(a, b, c)
+}
+
+// видео 45:00
