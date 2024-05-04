1ae5e4a631e0a1c1badbbc26c8d6a78807d75e0c
granolaa Asus TUF <m1907674@edu.misis.ru>
2024-05-04 21:45:07 +0300

    cycles started

--- main.go
+++ main.go
@@ -1,40 +1,77 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"log"
-)
+import "fmt"
 
 func main() {
-	name := "Max"
-	age := 52
-	new_m := sayHello(name, age)
-	mes, err := checkAge(age)
-	if err != nil {
-		log.Fatal(err)
-	}
-	printMessage(new_m)
-	printMessage(mes)
+	// name := "Max"
+	// age := 52
+	// new_m := sayHello(name, age)
+	// mes, err := checkAge(age)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// printMessage(new_m)
+	// printMessage(mes)
+	// dayOfweek := "asdad"
+	// fmt.Println(calendar(dayOfweek))
+
+	fmt.Println(finMin(1, 23, 23432, 0, -1, 1241))
 
 }
 
-func printMessage(message string) {
-	fmt.Println(message)
-}
-
-func sayHello(name string, age int) string {
-	return fmt.Sprintf("Привет, %s! Тебе %d лет.", name, age)
-}
-
-func checkAge(age int) (string, error) {
-	if age >= 18 && age < 45 {
-		return "Входи!", nil
-	} else if age >= 45 {
-		return "Не староват ли?", nil
+func finMin(nums ...int) int {
+	if len(nums) == 0 {
+		return 0
 	}
 
-	return "Слышь тебе сюда нельзя!", errors.New("you are too young for this")
+	min := nums[0]
+
+	for _, i := range nums {
+		if i < min {
+			min = i
+		}
+	}
+
+	return min
 }
+
+// func calendar(day string) (string, error) {
+// 	switch day {
+// 	case "пн":
+// 		return "Сегодня понедельник хорошего дня!", nil
+// 	case "вт":
+// 		return "Сегодня вторник хорошего дня!", nil
+// 	case "ср":
+// 		return "Сегодня среда хорошего дня!", nil
+// 	case "чт":
+// 		return "Сегодня четверг хорошего дня!", nil
+// 	case "пт":
+// 		return "Сегодня пятница хорошего дня и наступающих выходных!", nil
+// 	case "сб":
+// 		return "Сегодня суббота хороших выходных!", nil
+// 	case "вс":
+// 		return "Сегодня воскресенье, завтра на работу!", nil
+// 	default:
+// 		return "Неправильный ввод!", errors.New("incorrect input")
+// 	}
+// }
+
+// func printMessage(message string) {
+// 	fmt.Println(message)
+// }
+
+// func sayHello(name string, age int) string {
+// 	return fmt.Sprintf("Привет, %s! Тебе %d лет.", name, age)
+// }
+
+// func checkAge(age int) (string, error) {
+// 	if age >= 18 && age < 45 {
+// 		return "Входи!", nil
+// 	} else if age >= 45 {
+// 		return "Не староват ли?", nil
+// 	}
+
+// 	return "Слышь тебе сюда нельзя!", errors.New("you are too young for this")
+// }
 
 // видео 1:19:00
