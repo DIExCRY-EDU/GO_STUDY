741a4ba2e0c972321b970eff958003ae2f6bbcb3
granolaa Asus TUF <m1907674@edu.misis.ru>
2024-05-04 21:17:49 +0300

    Error type explained

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 )
 
 func main() {
 	name := "Max"
-	age := 16
+	age := 52
 	new_m := sayHello(name, age)
-	mes, _ := checkAge(age)
+	mes, err := checkAge(age)
+	if err != nil {
+		log.Fatal(err)
+	}
 	printMessage(new_m)
 	printMessage(mes)
 
@@ -22,12 +27,14 @@ func sayHello(name string, age int) string {
 	return fmt.Sprintf("Привет, %s! Тебе %d лет.", name, age)
 }
 
-func checkAge(age int) (string, bool) {
-	if age >= 18 {
-		return "Входи!", true
+func checkAge(age int) (string, error) {
+	if age >= 18 && age < 45 {
+		return "Входи!", nil
+	} else if age >= 45 {
+		return "Не староват ли?", nil
 	}
 
-	return "Слышь тебе сюда нельзя!", false
+	return "Слышь тебе сюда нельзя!", errors.New("you are too young for this")
 }
 
-// видео 1:10:00
+// видео 1:19:00
